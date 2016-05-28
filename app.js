1a58b8e6ee44736b0772394402bb999b2c9a487c
Manish Kumar <manish.dentalkart@gmail.com>
2016-05-29 01:10:37 +0530

    code modified

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = angular.module('app', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'views/home.html'
+        })
+        
+
+        .state('about', {
+            url: '/about',
+            templateUrl: 'views/about.html'
+        });
+
+});
---
+++ routes.js
@@ -0,0 +1,18 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
