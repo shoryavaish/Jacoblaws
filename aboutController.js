49258c6d3287a539508857a1f95b7a9a1ae19b75
Manish Kumar <manish.dentalkart@gmail.com>
2016-05-31 20:27:32 +0530

    modified for ajx

--- aboutController.js
+++ aboutController.js
@@ -1,6 +1,6 @@
 app.controller('aboutController', function($scope, $http) {
 	//here i ma pssing $http as dependency as i am going to use http get method for ajax
-   var url = "datafile/data.txt";
+   var url = "data/about.txt";
 
    $http.get(url).success( function(response) {
       $scope.aboutUsContent = response; 
